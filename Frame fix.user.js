3057ff9505dd2431ad2bfab2230fae0cec32e6df
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-02-15 22:34:44 -0500

    switched to regex @include

--- Frame fix.user.js
+++ Frame fix.user.js
@@ -2,7 +2,7 @@
 // @name         Frame fix
 // @description  frames link to monitor better
 // @author       SArpnt (+ p1)
-// @version      1.0.0
+// @version      1.0.1
 // @namespace    https://boxcrittersmods.ga/authors/sarpnt/
 // @homepage     https://boxcrittersmods.ga/mods/frame-fix/
 // @updateURL    https://github.com/SArpnt/Frame-fix/raw/master/Frame%20fix.user.js
@@ -10,11 +10,6 @@
 // @supportURL   https://github.com/SArpnt/Frame-fix/issues
 // @run-at       document-idle
 // @grant        none
-// @match        https://boxcritters.com/play/
-// @match        https://boxcritters.com/play/?*
-// @match        https://boxcritters.com/play/#*
-// @match        https://boxcritters.com/play/index.html
-// @match        https://boxcritters.com/play/index.html?*
-// @match        https://boxcritters.com/play/index.html#*
+// @include      /^https:\/\/boxcritters\.com\/play\/(index\.html)?([\?#].*)?$/
 // ==/UserScript==
 createjs.Ticker.timingMode = createjs.Ticker.RAF;
